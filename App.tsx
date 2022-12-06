880071a5801f26887ced068c615ae217822077eb
Marcos Lorhan <marcoslorhanbs@gmail.com>
2022-12-06 15:53:11 -0300

    Defining the Strucutre

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import {Text, View } from 'react-native';
+import {styles} from './themes/theme'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="#47B5FF" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
